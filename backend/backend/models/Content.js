0d87d041c1581879e3c34a33e855eaae4ec79425
Sekhsumon <techsumon18@gmail.com>
2025-06-11 04:37:50 +0530

    Update Content.js

--- backend/backend/models/Content.js
+++ backend/backend/models/Content.js
@@ -1,24 +1,10 @@
 const mongoose = require("mongoose");
 
 const contentSchema = new mongoose.Schema({
-  type: {
-    type: String,
-    required: true,
-  },
-  content: {
-    type: String,
-    required: true,
-  },
-  accessId: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    expires: 86400, // ২৪ ঘণ্টা পরে ডিলিট হবে (১ দিন)
-  },
+  type: { type: String, required: true },
+  content: { type: String, required: true },
+  accessId: { type: String, required: true, unique: true },
+  createdAt: { type: Date, default: Date.now, expires: 86400 }
 });
 
 module.exports = mongoose.model("Content", contentSchema);
